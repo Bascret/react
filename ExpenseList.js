5c0ce20e0826df40d39aaeb0bb56e5cd845cafee
Bascret <161457134+Bascret@users.noreply.github.com>
2024-10-24 01:29:21 +0900

    Add files via upload

--- ExpenseList.js
+++ ExpenseList.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import './ExpenseList.css';
 import ExpenseItem from './ExpenseItem';
+import { MdDelete } from 'react-icons/md';
 
-const ExpenseList = ({initialExpenses, handleDelete}) => {
+const ExpenseList = ({ expenses, initialExpenses, handleDelete, handleEdit, clearItems }) => {
   return (
     <>
       <ul className='list'>
@@ -10,15 +11,22 @@ const ExpenseList = ({initialExpenses, handleDelete}) => {
           return (
             <ExpenseItem key={expense.id} expense={expense}
 
-              handleDelete={handleDelete}
+              handleDelete={handleDelete} handleEdit={handleEdit}
             />
           )
         })}
 
       </ul>
-      <button className='btn'>
-        목록 지우기
-      </button>
+      {expenses.length > 0 ?
+        <button className='btn'
+          onClick={clearItems}>
+          목록 지우기
+          <MdDelete className='btn-icon'/>
+        </button>
+        : null
+
+      }
+
     </>
   )
 }
